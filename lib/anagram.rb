4b388919ace4991fee6e03145e41838ef0ff3a90
Carmen Renee <carmengenpt@gmail.com>
2019-02-13 15:28:11 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,8 +7,8 @@ class Anagram
     @word = word
   end 
   
-  def match(word_array)
-    word_array.select do |word|
+  def match(array)
+    array.select do |word|
     word.split("").sort == @world.split("").sort
   end
  end
