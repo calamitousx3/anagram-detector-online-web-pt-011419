487cb49699ce01ff8801eb21d5f048f1ad12d933
Carmen Renee <carmengenpt@gmail.com>
2019-02-13 15:02:27 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -14,3 +14,16 @@ end
 
 
 #[1,2,3,4,5].select { |num|  num.even?  } 
+
+class Loan
+    def initialize(amount, interest)
+        @amount = amount
+        @interest = interest
+    end
+end
+
+loan1 = Loan.new(100, 0.1)
+
+Loan.each do |amount, interest|
+    debt = debt + amount + (amount*interest)
+end
