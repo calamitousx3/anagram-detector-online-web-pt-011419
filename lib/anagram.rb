3fbc7c26c9be59f979c1d0cf02a5ddb7488b0b4c
Carmen Renee <carmengenpt@gmail.com>
2019-02-13 15:10:10 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,7 +8,7 @@ class Anagram
   end 
   
   def match(array)
-    array.select {|first|
+    array.select do |word|
     
   end
   
@@ -16,16 +16,6 @@ end
 
 
 
-#[1,2,3,4,5].select { |num|  num.even?  } 
-
-class Loan
-    def initialize(amount, interest)
-        @amount = amount
-        @interest = interest
-    end
-end
-
-loan1 = Loan.new(100, 0.1)
 
 Loan.each do |amount, interest|
     debt = debt + amount + (amount*interest)
